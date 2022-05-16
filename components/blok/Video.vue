a52887e5230523261c89bc312fe911c5ec74f619
vejron <bjorn.yttergren@gmail.com>
2022-05-16 14:21:12 +0200

    feat: video rich-text-component

---
+++ components/blok/Video.vue
@@ -0,0 +1,25 @@
+<template>
+  <div v-editable="blok">
+    <iframe
+      v-if="blok.youtube_video_id"
+      class="w-full min-h-50 sm:min-h-sm"
+      :src="`https://www.youtube.com/embed/${blok.youtube_video_id}`"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <div class="w-full min-h-sm grid place-content-center text-red-500 font-bold text-2xl" v-else>
+      No Youtube id found
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  blok: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
--- plugins/richtextRenderer.ts
+++ plugins/richtextRenderer.ts
@@ -1,6 +1,19 @@
-import { defineNuxtPlugin } from '#app'
-import { plugin } from '@marvr/storyblok-rich-text-vue-renderer'
+import { defineNuxtPlugin } from "#app";
+import {
+  defineResolvers,
+  plugin,
+} from "@marvr/storyblok-rich-text-vue-renderer";
+import BlokVideo from "~/components/blok/Video.vue";
 
 export default defineNuxtPlugin(({ vueApp }) => {
-	vueApp.use(plugin(/* options */))
-})
+  vueApp.use(
+    plugin({
+      resolvers: defineResolvers({
+        components: {
+          "blok-video": ({ fields }) =>
+            h(BlokVideo, { blok: { youtube_video_id: fields.youtube_video_id} }),
+        },
+      }),
+    })
+  );
+});
